b11af7eb7af5483f4cfacf6d1c2b72e8e6631e7d
Tommy-dotcom <totolamothe65@gmail.com>
2023-05-22 23:26:40 +0200

    About paragraph

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,18 +1,67 @@
 <template>
-    <div>
-        <img src="../../assets/img/about_me.jpg" alt="Thomas Lamothe">
-        <h1>Salut le router 🤘</h1>
+    <div id="aboutMe">
+        <div class="left">
+            <img src="../../assets/img/about_me.jpg" alt="Thomas Lamothe">
+        </div>
+        <div class="right">
+            <h1>Bonjour <span @mouseover=randomEmoji() @mouseleave="this.emoji = '🤘'" class="finger">{{ emoji }}</span></h1>
+
+            <p>Je suis Thomas, ceci est mon site web. Pas mal non ? C'est... français.</p>
+            <p>Je me permets d'interrompre votre navigation parce qu'on se fout un peu de ma gueule... C'est du vol et du plagiat. J'aime pas trop les voleurs et les... fils de putes.</p>
+
+            <p class="high-margin">Je suis Lead Développeur chez <a :href=iwitLink target="_blank">Iwit Systems</a> à Toulouse, France.</p>
+            <p>Mon travail consiste essentiellement dans le développement d'applications web métier complexes. Je bosse majoritairement avec 
+                <a target="_blank" :href=symfonyLink>Symfony</a>, 
+                <a target="_blank" :href=vueLink>Vue.js</a> et <a target="_blank" :href=dockerLink>Docker</a>.
+            </p>
+
+            <p class="high-margin">Je suis également à l'initiative de projets comme <a :href="traxLink" target="_blank">Trax</a>, Orkestrum, Cinécombles et de nombreux autres qui ne verront jamais le jour.</p>
+            <p>Passionné par la culture underground, je fais de la musique (guitare) depuis {{ guitarYears }} ans, aime me déplacer en skate, les concerts, les tatouages, les vides greniers, les films des frères Coen, les fringues et la bière.</p>
+            <p>Tu peux me croiser facilement sur Toulouse et cela serait un plaisir d'échanger avec toi.</p>
+        </div>
     </div>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            emoji: "🤘",
+            emojisArray: ["✊", "🤘", "🤙", "✌", "🖕"],
+            iwitLink: 'https://iwit-systems.fr',
+            symfonyLink: 'https://symfony.com',
+            vueLink: 'https://vuejs.org',
+            dockerLink: 'https://docker.com',
+            traxLink: 'https://trax.thomaslamothe.com',
+        };
+    },
+    computed: {
+        guitarYears() {
+            return new Date().getFullYear() - 2014;
+        }
+    },
+    methods: {
+        randomEmoji() {
+            this.shuffleEmojiArray().forEach((e, index) => {                
+                setTimeout(() => {
+                    this.emoji = e;
+                }, (index + 1) * 500);
+            });
+        }, 
+        shuffleEmojiArray() {
+            let currentIndex = this.emojisArray.length, randomIndex;
 
-</script>
+            while (currentIndex != 0) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
 
-<style scoped>
-img {
-    width: 30%;
-    border-radius: 40px;
-    box-shadow: 0 0 50px rgba(0,0,0,.2);
+                [this.emojisArray[currentIndex], this.emojisArray[randomIndex]] = [
+                    this.emojisArray[randomIndex], this.emojisArray[currentIndex]
+                ];
+            }
+
+            return this.emojisArray;
+        }
+    }
 }
-</style>
+</script>
