707d50a4bb1138435aae41646e013ae6166fcdf4
Tommy-dotcom <totolamothe65@gmail.com>
2023-06-07 08:37:52 +0200

    Env variables

---
+++ src/components/culture/Culture.vue
@@ -0,0 +1,29 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            movies: []
+        };
+    },
+    mounted() {
+        console.log(import.meta.env);
+        // this.fetchMovies();
+    },
+    methods: {
+        // async fetchMovies() {
+        //     const movies = fetch()
+        // }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+main {
+    width: 0;
+    padding: 0;
+}
+</style>
