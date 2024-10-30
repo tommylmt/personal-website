b74be55ad5639a993ceeb81ccdbafb1f72534102
Tommy-dotcom <totolamothe65@gmail.com>
2024-10-30 08:34:05 +0100

    feat: replace existing with Tailwind + improve global style

--- src/components/menu/MenuItem.vue
+++ src/components/menu/MenuItem.vue
@@ -5,7 +5,7 @@
         @mouseover="onHover()"
         @mouseleave="stopHover()"
     >
-        <router-link :to="path" @click="changeActive()" ref="link">{{ name }}</router-link>
+        <router-link :to="path" @click="changeActive()" ref="link" class="font-sans font-light">{{ name }}</router-link>
     </li>
 </template>
 
@@ -13,8 +13,8 @@
 export default {
     data() {
         return {
-            mainClass: 'menu-item',
-            currentClass: 'current-item'
+            mainClass: 'font-sans px-5 py-2 relative hover:text-white transition-color duration-300',
+            currentClass: 'current-item text-white'
         }
     },
     props: ['path', 'current', 'name'],
