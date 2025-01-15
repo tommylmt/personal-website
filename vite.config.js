06883eef303834f5871e413370d2ba0b3a85c646
Tommy-dotcom <totolamothe65@gmail.com>
2025-01-15 08:38:49 +0100

    chore: update vue + ejs syntax in index file for production specificities

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      ViteEjsPlugin(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
