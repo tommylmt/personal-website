b8d37569fde51fa42e4f844c053517abc0bfe7e1
Tommy-dotcom <totolamothe65@gmail.com>
2025-10-27 20:08:21 +0100

    style: improving dark theme wip

--- src/components/errors/Error404.vue
+++ src/components/errors/Error404.vue
@@ -3,17 +3,32 @@
         <ImageTrailCursor v-if="images.length > 0" :images="images" />
         <div class="absolute flex w-full top-0 h-full bottom-0 left-0 right-0 justify-center items-center">
             <div class="text-center">
-                <h1 class="text-9xl text-black font-black uppercase">{{ $t('error.nope') }}</h1>
-                <span class="font-mono bg-slate-100 rounded-md border border-slate-200 px-2">{{ $t('error.error') }} 404</span>
-                <p class="text-slate-400 intersect-once intersect:motion-translate-x-out-25 intersect:motion-opacity-in-0">
+                <h1 class="text-9xl text-black font-black uppercase dark:text-white">{{ $t('error.nope') }}</h1>
+                <span
+                    :class="[
+                        'font-mono bg-slate-100 rounded-md border border-slate-200 px-2',
+                        'dark:bg-slate-800 dark:border-slate-700 dark:text-white'
+                    ]"
+                >
+                    {{ $t('error.error') }} 404
+                </span>
+                <p :class="['text-slate-400 intersect-once intersect:motion-translate-x-out-25 intersect:motion-opacity-in-0', 'dark:']">
                     Mmhhh.... It seems the page you are looking for does not exists. 🧐
                 </p>
 
                 <div class="flex justify-center mt-10">
                     <router-link to="/" ref="link">
-                        <ShimmerButton class="shadow-2xl scale-100 hover:scale-105" shimmer-size="2px">
+                        <ShimmerButton
+                            class="shadow-2xl scale-100 hover:scale-105"
+                            shimmer-size="2px"
+                            shimmer-color="#000000BB"
+                            background="#FFFFFF"
+                        >
                             <span
-                                class="whitespace-pre-wrap text-center font-medium leading-none tracking-tight text-white lg:text-lg dark:from-white dark:to-slate-900/10 text-sm"
+                                :class="[
+                                    'whitespace-pre-wrap text-center font-medium leading-none tracking-tight text-white lg:text-lg dark:from-white dark:to-slate-900/10 text-sm',
+                                    'dark:text-black'
+                                ]"
                             >
                                 Take me home
                             </span>
