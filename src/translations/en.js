7ad8a828d30a792e760c4f7cb3fb3b12e898e475
Tommy-dotcom <totolamothe65@gmail.com>
2026-01-04 19:17:39 +0100

    fix: improve lighthouse score + minor change in meta sentences

--- src/translations/en.js
+++ src/translations/en.js
@@ -33,7 +33,7 @@ export const en = {
     },
     seo: {
         title: {
-            about: 'Thomas Lamothe - Software Engineer',
+            about: 'Thomas Lamothe - Software Developer',
             stack: 'My Stack',
             culture: 'Things I love',
             notfound: 'Resource not found'
@@ -41,16 +41,16 @@ export const en = {
         meta: {
             about: {
                 description:
-                    "I'm Thomas, a french web software engineer. I help design, build, and maintain a globally deployed, multi-tenant SaaS platform.",
+                    "I'm Thomas, a french web software developer. I help design, build, and maintain a globally deployed, multi-tenant SaaS platform.",
                 twitter: {
-                    title: 'Thomas Lamothe - Software Engineer',
+                    title: 'Thomas Lamothe - Software Developer',
                     description:
-                        "I'm Thomas, a french software engineer. I help design, build, and maintain a globally deployed, multi-tenant SaaS platform."
+                        "I'm Thomas, a french software developer. I help design, build, and maintain a globally deployed, multi-tenant SaaS platform."
                 },
                 og: {
-                    title: 'Thomas Lamothe - Software Engineer',
+                    title: 'Thomas Lamothe - Software Developer',
                     description:
-                        "I'm Thomas, a french software engineer. I help design, build, and maintain a globally deployed, multi-tenant SaaS platform."
+                        "I'm Thomas, a french software developer. I help design, build, and maintain a globally deployed, multi-tenant SaaS platform."
                 }
             },
             stack: {
