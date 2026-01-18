23d738cb9fdb2f76040ac0ae7ef9cabf33420b25
Tommy-dotcom <totolamothe65@gmail.com>
2026-01-18 15:29:53 +0100

    feat(menu): converting vue into React Ts code

--- next/src/components/menu/MenuItem.tsx
+++ next/src/components/menu/MenuItem.tsx
@@ -1,6 +1,11 @@
 import {ReactElement} from "react";
 import {NavItem} from "@/src/types/menu";
 
-export default function MenuItem({ page }: { page: NavItem }): ReactElement {
+export default function MenuItem({ page, onFollowItem, onHoverStop, onActiveChange }: {
+    page: NavItem,
+    onFollowItem: (element: HTMLElement) => void,
+    onHoverStop: () => void,
+    onActiveChange: (path: string) => void
+}): ReactElement {
 
 }
