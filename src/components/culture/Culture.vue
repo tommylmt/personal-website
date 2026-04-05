7f10728faa7abb4c3ad3470af6d58fbac8268dd6
Tommy-dotcom <totolamothe65@gmail.com>
2026-04-05 14:50:35 +0200

    refactor(culture): migrate Culture.vue to Composition API

--- src/components/culture/Culture.vue
+++ src/components/culture/Culture.vue
@@ -78,74 +78,54 @@
     <CultureDetailModal />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { Vue3Marquee } from 'vue3-marquee'
 import CulturePoster from '@/components/culture/CulturePoster.vue'
 import MeetMe from '@/components/culture/MeetMe.vue'
 import Music from '@/components/culture/Music.vue'
 import ErrorBanner from '@/components/errors/ErrorBanner.vue'
-import { CULTURE_LINKS, MEDIA_TYPE } from '@/utils/constants'
 import CultureDetailModal from '@/components/culture/CultureDetailModal.vue'
-import type { SpotifySong, TCultureData, TCultureElement } from '@/types/culture.ts'
 import ContainerLayout from '@/components/layout/ContainerLayout.vue'
+import type { SpotifySong, TCultureElement } from '@/types/culture.ts'
+import { CULTURE_LINKS, MEDIA_TYPE } from '@/utils/constants'
+import { computed, onMounted, ref } from 'vue'
 import { useApiClient } from '@/composables/useApiClient.ts'
 
-// TODO migrate this component to composition API
-let requester = null
+const movies = ref<TCultureElement[]>([])
+const shows = ref<TCultureElement[]>([])
+const errors = ref<false | string>(false)
+const errorSongs = ref<false | string>(false)
+const songs = ref<SpotifySong[]>([])
 
-export default {
-    setup() {
-        const { apiRequest } = useApiClient()
-        requester = apiRequest
-    },
-    components: {
-        ContainerLayout,
-        CultureDetailModal,
-        ErrorBanner,
-        Music,
-        MeetMe,
-        CulturePoster,
-        Vue3Marquee
-    },
-    data(): TCultureData {
-        return {
-            movies: [],
-            shows: [],
-            errors: false,
-            errorSongs: false,
-            songs: []
-        }
-    },
-    computed: {
-        links() {
-            return CULTURE_LINKS
-        }
-    },
-    mounted() {
-        this.fetchElements()
-        this.retrieveCharts()
-    },
-    methods: {
-        speedForElements(elements: TCultureElement[]) {
-            return (20 / 5) * elements.length
-        },
-        async fetchElements() {
-            try {
-                const data = await requester<TCultureElement[]>('/api/culture')
+const { apiRequest } = useApiClient()
 
-                this.movies = data.filter((media) => media.media_type.slug === MEDIA_TYPE.Movie)
-                this.shows = data.filter((media) => media.media_type.slug === MEDIA_TYPE.TvShows)
-            } catch (_) {
-                this.errors = 'culture.errors.fetching'
-            }
-        },
-        async retrieveCharts() {
-            try {
-                this.songs = await requester<SpotifySong[]>('/api/charts')
-            } catch (_) {
-                this.errorSongs = 'culture.errors.charts'
-            }
-        }
+const links = computed(() => CULTURE_LINKS)
+
+onMounted(() => {
+    fetchElements()
+    retrieveCharts()
+})
+
+const speedForElements = (elements: TCultureElement[]) => {
+    return (20 / 5) * elements.length
+}
+
+const fetchElements = async () => {
+    try {
+        const data = await apiRequest<TCultureElement[]>('/api/culture')
+
+        movies.value = data.filter((media) => media.media_type.slug === MEDIA_TYPE.Movie)
+        shows.value = data.filter((media) => media.media_type.slug === MEDIA_TYPE.TvShows)
+    } catch (_) {
+        errors.value = 'culture.errors.fetching'
+    }
+}
+
+const retrieveCharts = async () => {
+    try {
+        songs.value = await apiRequest<SpotifySong[]>('/api/charts')
+    } catch (_) {
+        errorSongs.value = 'culture.errors.charts'
     }
 }
 </script>
