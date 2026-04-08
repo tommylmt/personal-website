606a1d6ecb867eac9ad86f7e482962c2e2f81cf2
Tommy-dotcom <totolamothe65@gmail.com>
2026-04-08 13:08:00 +0200

    style(colors): upgrade colors for better contrast and more modern look

--- src/components/about/MyLife.vue
+++ src/components/about/MyLife.vue
@@ -7,8 +7,8 @@
         }"
     >
         <div class="p-6 bg-neutral-100/70 backdrop-blur-lg w-80 text-center dark:bg-neutral-900/70">
-            <h3 class="font-extrabold font-sans text-neutral-700 text-3xl dark:text-neutral-300">🍻 {{ $t('about.earth.title') }}</h3>
-            <p class="text-neutral-600">{{ $t('about.earth.subtitle') }}</p>
+            <h3 class="font-extrabold font-sans text-neutral-800 text-3xl dark:text-neutral-300">🍻 {{ $t('about.earth.title') }}</h3>
+            <p class="text-neutral-700">{{ $t('about.earth.subtitle') }}</p>
         </div>
     </DraggableBlock>
 </template>
--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -2,7 +2,7 @@
     <div
         :class="[
             'group relative hover:shadow-lg border rounded-3xl cursor-help overflow-hidden my-7 xl:my-0',
-            'h-[500px] md:h-[275px] xl:h-[375px] 2xl:h-[500px] w-full md:w-[48%] lg:w-[22%]',
+            'h-125 md:h-68.75 xl:h-93.75 2xl:h-125 w-full md:w-[48%] lg:w-[22%]',
             'border-neutral-50 dark:border-neutral-900'
         ]"
         data-aos="fade-up"
@@ -19,7 +19,7 @@
         ></div>
         <div
             :class="[
-                'absolute transition-all duration-200 content bg-white/90 backdrop-blur-md p-5 rounded-2xl w-[96%] left-[2%] -bottom-[200px] -xl:bottom-[160px] group-hover:bottom-2 dark:bg-neutral-950/80',
+                'absolute transition-all duration-200 content bg-white/90 backdrop-blur-md p-5 rounded-2xl w-[96%] left-[2%] -bottom-50 -xl:bottom-[160px] group-hover:bottom-2 dark:bg-neutral-950/80',
                 { 'bottom-2': isExpanded }
             ]"
         >
